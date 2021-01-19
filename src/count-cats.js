1a2590a69e61946157a70095ac1f889c6fafe9b7
s1ga <s.v.harlanov@gmail.com>
2021-01-19 16:48:58 +0300

    fix: fix passing date

--- src/count-cats.js
+++ src/count-cats.js
@@ -4,7 +4,7 @@ module.exports = function countCats(backyard) {
   const catSign = '^^'
   let cats = 0
 
-  backyard.forEach(yard => yard.forEach(item => item === catSign ? cats++ : null))
+  backyard.forEach(yard => yard.forEach(item => item === catSign ? cats += 1 : null))
 
   return cats
 };
