501cee4f26719ea22822620914992df07a388a8e
s1ga <s.v.harlanov@gmail.com>
2021-01-20 18:27:38 +0300

    feat: implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  str = String(str)
+  options.addition = String(options.addition) !== 'undefined' ? String(options.addition) : ''
+  options.separator = options.separator ? options.separator : '+'
+  options.additionSeparator = options.additionSeparator ? options.additionSeparator : '|'
   
+  let newStr = ''
+  let addition = ''
+  if (options.additionRepeatTimes) {
+   for (let i = 1; i < options.additionRepeatTimes; i++) {
+    addition += options.addition + options.additionSeparator
+   } 
+  } 
+  addition += options.addition
+
+  if (options.repeatTimes) {
+    for (let i = 1; i <= options.repeatTimes; i++) {
+      newStr += str + addition
+      if (!(i === options.repeatTimes)) {
+        newStr += options.separator
+      }
+    } 
+  } else {
+    newStr = str + addition
+  }
+
+  return newStr
+}
+
